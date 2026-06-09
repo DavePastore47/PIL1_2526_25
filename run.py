7442988d43d1a8cf3a1512e1cc194f5ca0568160
Yannick <yannickassangbe15@gmail.com>
2026-06-09 15:05:31 +0100

    fix: autorise werkzeug pour le debug socketio

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app.routes import socket_events
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
